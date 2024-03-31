208f8fd1edaf331cfd815a64e64b6f72fe8ae3cf
ricardoazev <ricardometronome@gmail.com>
2024-03-30 22:31:48 -0300

    Implementando delete

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -12,6 +13,7 @@ public class Program {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
 	
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
@@ -45,6 +47,15 @@ public class Program {
 		seller.setName("Martha Waine");
 		sellerDao.update(seller);
 		System.out.println("Update completed");
+		
+		System.out.println("\n==== TESTE 6: Implementando delete ====");
+		System.out.println("Enter id for delete test: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete completed");
+		
+		sc.close();
+		
 
 	}
 
